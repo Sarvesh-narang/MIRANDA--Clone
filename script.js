39a123fdf812f32a009b933fa4ce682f282b9ac0
Sarvesh Narang <111209386+Sarvesh-narang@users.noreply.github.com>
2024-04-11 07:25:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+// window.onload  = function() {
+//     window.scrollTo(0,0);
+// };
+// window.onbeforeunload = function () {
+//     window.scrollTo(0, 0);
+// };
+
+
+let tl = gsap.timeline();
+
+tl.to("#page1", {
+    y:"100vh",
+    scale: 0.6,
+    duration: 0
+})
+tl.to("#page1", {
+    y:"30vh",
+    duration: 1,
+    delay:1
+})
+
+tl.to("#page1",{
+    y:"0vh",
+    rotate:"360",
+    scale: 1,
+    duration:0.7
+})
